8c1a4cd5721d00a55bad78155ed3d41f0905a9be
Sittichok054 <aquasilvia2547@gmail.com>
2025-12-09 00:56:09 -0800

    lab4

---
+++ LAB4/web/page/blog.php
@@ -0,0 +1,13 @@
+<?php
+    echo "I LOVE IT";
+
+    $name = "Nathan";
+    $age = 18;
+
+    echo $name. "".$age;
+
+    $arr = [1,2,"test"];
+    var_dump($arr[2]);
+
+?>
+<?= $name . " " .$age ?>
